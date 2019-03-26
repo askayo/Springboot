62df2602e31c295bfed2e48fabc6318cce6f5779
sovan <sovanlong@gmail.com>
2019-03-26 23:59:04 +0100

    td5 avec quelques templatee

--- td5/src/main/java/s4/spring/td5/controllers/UserController.java
+++ td5/src/main/java/s4/spring/td5/controllers/UserController.java
@@ -1,9 +1,17 @@
 package s4.spring.td5.controllers;
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.view.RedirectView;
 
 import s4.spring.td5.entities.User;
 import s4.spring.td5.repositories.UserRepository;
@@ -29,6 +37,40 @@ public class UserController {
 		return user.getIdentity() +" ajouté dans la bdd.";
 		
 	}
+	
+	
+	@RequestMapping("{path:index}")
+	public String index(Model model) {
+		List<User> users = userRep.findAll();
+		model.addAttribute("users", users);
+		return"index";
+	}
+	
+	
+	@PostMapping("login")
+	public RedirectView Login(Model model, User user, HttpSession session) {
+		userRep.findAll();
+		
+		return new RedirectView("/login");
+	}
+	
+	
+	@GetMapping("login")
+	public String loginView(Model model) {
+		model.addAttribute("user", new User());
+		return "login";
+	}
+	
+
+	@RequestMapping("logout")
+	public String logout() {
+	
+		return "logout";
+	}
+	
+	
+	
+	
 	
 	
 	
