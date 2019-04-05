f697dd49cff817c0a6b9d9b6581dee7e0895e155
sovan <sovanlong@gmail.com>
2019-04-05 19:57:37 +0200

    fonction delete script fonctionne

--- td5/src/main/java/s4/spring/td5/controllers/ScriptController.java
+++ td5/src/main/java/s4/spring/td5/controllers/ScriptController.java
@@ -1,27 +1,35 @@
 package s4.spring.td5.controllers;
 
+import java.sql.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
+import s4.spring.td5.entities.History;
 import s4.spring.td5.entities.Script;
 import s4.spring.td5.repositories.CategoryRepository;
+import s4.spring.td5.repositories.HistoryRepository;
 import s4.spring.td5.repositories.LanguageRepository;
 import s4.spring.td5.repositories.ScriptRepository;
 import s4.spring.td5.repositories.UserRepository;
 
 @Controller
-@RequestMapping("/script/")
+
 public class ScriptController {
 	
 	@Autowired
 	private ScriptRepository scrRep;
-
+	private HistoryRepository historyRepo;
 	
-	@PostMapping("createScript")
+	@PostMapping("/script/createScript")
 	public RedirectView createScript(Script script) {
 		scrRep.save(script);
 		return new RedirectView("../user/index");
@@ -29,11 +37,19 @@ public class ScriptController {
 		
 	}
 	
-	@GetMapping("new")
+	@GetMapping("/script/new")
 	public String newScript() {
 		return "script/new";
 	}
 	
+	@GetMapping("user/script/{id}/delete")
+	public RedirectView deleteScript(@PathVariable("id") int id, ModelMap model) {
+			scrRep.deleteById(id);
+			return new RedirectView("../../index");
+
+	}
+	
+
 	
 
 }
--- td5/src/main/java/s4/spring/td5/controllers/UserController.java
+++ td5/src/main/java/s4/spring/td5/controllers/UserController.java
@@ -7,7 +7,9 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,7 +28,7 @@ public class UserController {
 
 	@Autowired
 	private UserRepository userRep;
-	
+	private ScriptRepository scrRep;
 	
 	@Autowired
 	private ScriptRepository scriptRep;
@@ -83,6 +85,8 @@ public class UserController {
 	
 		return "logout";
 	}
+	
+	
 	
 	
 	
